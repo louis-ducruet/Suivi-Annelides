9e122101624c23dc226f8fdef3ba0716529e6f73
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-03-01 20:29:26 +0100

    Add footer.php

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 $title = "Accueil";
 $root_path = "./";
 include_once "src/layout/header.php";
+include_once "src/layout/footer.php";
---
+++ src/layout/footer.php
@@ -0,0 +1,16 @@
+</main>
+<footer id="footer" class="mt-5">
+    <div class="container my-3 text-center">
+        <span>
+            <?php
+            if (date("Y") != 2022) {
+                echo "© Copyright 2022 - " . date("Y") . " | Site créé par DUCRUET Louis";
+            } else {
+                echo "© Copyright 2022 | Site créé par DUCRUET Louis";
+            }
+            ?>
+        </span>
+    </div>
+</footer>
+</body>
+</html>
