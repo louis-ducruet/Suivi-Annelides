e5b2505d4545ef7806fa58c891277cac382add39
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-03-03 21:02:53 +0100

    Update coming soon msg

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ $root_path = "./";
 include_once "src/layout/header.php";
 ?>
 <section>
-    <h1>Site en cours de développement</h1>
+    <div class="container mt-5">
+        <h1>Site en cours de développement !</h1>
+        <a href="/admin">Liste des pages codés</a>
+    </div>
 </section>
 <?php
 include_once "src/layout/footer.php";
